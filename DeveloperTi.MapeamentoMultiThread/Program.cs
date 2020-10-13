6fd35ed6f4c91e2d35cef9823bac7624178b6d1d
Tiago Rodrigues <tiago.rds85@hotmail.com>
2020-10-13 15:30:49 -0300

    Adicionar arquivos de projeto.

---
+++ DeveloperTi.MapeamentoMultiThread/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeveloperTi.MapeamentoMultiThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
